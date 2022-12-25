628e893965d5ec2f45c9b4b190319ef2bb4d126d
Gareth Adams <g@rethada.ms>
2022-12-25 17:30:46 +0000

    Improve system test execution
    * Switch to MiniTest syntax
    * Fix failure screenhot filenames
    * Make the tests pass

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,23 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1200, 900]
+
+  # Use this class for tests `describe`d with :system
+  register_spec_type(self) do |_desc, *addl|
+    addl.include? :system
+  end
 end
+
+# Rails system test failure screenshots are based on the generated method name,
+# which for MiniTest tests retain the spaces from the test description. When the
+# image filename is output into the terminal, the spaces mean it's not clickable
+# to open.
+#
+# This module overrides the screenshot filename helper to make it clickable
+module ClickableScreenshotFilenames
+  def image_name
+    super.gsub(/\s+/, "_")
+  end
+end
+
+ActionDispatch::SystemTesting::TestHelpers::ScreenshotHelper.prepend ClickableScreenshotFilenames
--- test/system/streams_test.rb
+++ test/system/streams_test.rb
@@ -1,43 +1,44 @@
 require "application_system_test_case"
 
-class StreamsTest < ApplicationSystemTestCase
-  setup do
-    @stream = create(:stream)
-  end
+describe "Stream management", :system do
+  let(:channel) { create(:twitch_channel) }
+  let(:stream) { create(:stream) }
 
-  test "visiting the index" do
+  it "allows listing streams" do
     visit streams_url
     assert_selector "h1", text: "Streams"
   end
 
-  test "should create stream" do
+  it "allows creating streams with the default channel" do
+    new_stream_params = build(:stream, twitch_channel: channel)
+
     visit streams_url
     click_on "New stream"
 
-    fill_in "Initial duration", with: @stream.initial_duration
-    fill_in "Starts at", with: @stream.starts_at
-    fill_in "Twitch channel", with: @stream.twitch_channel_id
+    fill_in "Initial duration", with: new_stream_params.initial_duration
+    fill_in "Starts at", with: new_stream_params.starts_at
+    fill_in "Twitch channel", with: new_stream_params.twitch_channel_id
     click_on "Create Stream"
 
     assert_text "Stream was successfully created"
     click_on "Back"
   end
 
-  test "should update Stream" do
-    visit stream_url(@stream)
+  it "allows updating streams" do
+    visit stream_url(stream)
     click_on "Edit this stream", match: :first
 
-    fill_in "Initial duration", with: @stream.initial_duration
-    fill_in "Starts at", with: @stream.starts_at
-    fill_in "Twitch channel", with: @stream.twitch_channel_id
+    fill_in "Initial duration", with: stream.initial_duration
+    fill_in "Starts at", with: stream.starts_at
+    fill_in "Twitch channel", with: stream.twitch_channel_id
     click_on "Update Stream"
 
     assert_text "Stream was successfully updated"
     click_on "Back"
   end
 
-  test "should destroy Stream" do
-    visit stream_url(@stream)
+  it "allows destroying streams" do
+    visit stream_url(stream)
     click_on "Destroy this stream", match: :first
 
     assert_text "Stream was successfully destroyed"
