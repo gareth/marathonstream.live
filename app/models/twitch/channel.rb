30ab4cd3f521ecb76d250a58f03a1108d28ac2e3
Gareth Adams <g@rethada.ms>
2022-12-29 13:04:02 +0000

    Tweak channel display

--- app/models/twitch/channel.rb
+++ app/models/twitch/channel.rb
@@ -2,8 +2,7 @@ module Twitch
   class Channel < ApplicationRecord
     has_many :streams, foreign_key: :twitch_channel_id
 
-    def to_param
-      twitch_username.to_param
-    end
+    delegate :to_param, to: :twitch_username
+    delegate :to_s, to: :twitch_display_name
   end
 end
