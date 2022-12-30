2f3093f59da00c101b4d501cde7a7f3851af402f
Gareth Adams <g@rethada.ms>
2022-12-30 10:22:55 +0000

    Update StreamsControllerTest to use minitest/spec syntax

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -1,22 +1,22 @@
 require "test_helper"
 
-class StreamsControllerTest < ActionDispatch::IntegrationTest
+describe StreamsController do
   let(:channel) { create(:twitch_channel) }
   setup do
     @stream = create(:stream, twitch_channel: channel)
   end
 
-  test "should get index" do
+  it "should get index" do
     get streams_url(subdomain: channel)
     assert_response :success
   end
 
-  test "should get new" do
+  it "should get new" do
     get new_stream_url(subdomain: channel)
     assert_response :success
   end
 
-  test "should create stream" do
+  it "should create stream" do
     assert_difference("Stream.count") do
       post streams_url(subdomain: channel),
            params: { stream: { initial_duration: @stream.initial_duration, starts_at: @stream.starts_at,
@@ -26,24 +26,24 @@ class StreamsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to stream_url(Stream.last, subdomain: channel)
   end
 
-  test "should show stream" do
+  it "should show stream" do
     get stream_url(@stream, subdomain: channel)
     assert_response :success
   end
 
-  test "should get edit" do
+  it "should get edit" do
     get edit_stream_url(@stream, subdomain: channel)
     assert_response :success
   end
 
-  test "should update stream" do
+  it "should update stream" do
     patch stream_url(@stream, subdomain: channel),
           params: { stream: { initial_duration: @stream.initial_duration, starts_at: @stream.starts_at,
                               twitch_channel_id: @stream.twitch_channel_id } }
     assert_redirected_to stream_url(@stream, subdomain: channel)
   end
 
-  test "should destroy stream" do
+  it "should destroy stream" do
     assert_difference("Stream.count", -1) do
       delete stream_url(@stream, subdomain: channel)
     end
