ef0121c6e02d3f925272b4d323460fa17c37cd8a
Gareth Adams <g@rethada.ms>
2023-01-05 02:12:18 +0000

    Move system tests to controller tests
    I did a bunch of catching up what the difference between system,
    controller and integration tests were. Everything I want to do here is
    better off as a controller test.

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -2,52 +2,45 @@ require "test_helper"
 
 describe StreamsController do
   let(:channel) { create(:twitch_channel) }
-  setup do
-    @stream = create(:stream, twitch_channel: channel)
+
+  describe "with no streams" do
+    describe "#index" do
+      it "allows creation of a stream"
+    end
   end
 
-  it "should get index" do
-    get streams_url(subdomain: channel)
-    assert_response :success
-  end
-
-  it "should get new" do
-    get new_stream_url(subdomain: channel)
-    assert_response :success
-  end
-
-  it "should create stream" do
-    assert_difference("Stream.count") do
-      post streams_url(subdomain: channel),
-           params: { stream: { initial_duration: @stream.initial_duration, starts_at: @stream.starts_at,
-                               twitch_channel_id: @stream.twitch_channel_id } }
+  describe "with a stream" do
+    before do
+      @stream = create(:stream, twitch_channel: channel)
     end
 
-    assert_redirected_to stream_url(Stream.last, subdomain: channel)
-  end
-
-  it "should show stream" do
-    get stream_url(@stream, subdomain: channel)
-    assert_response :success
-  end
-
-  it "should get edit" do
-    get edit_stream_url(@stream, subdomain: channel)
-    assert_response :success
-  end
-
-  it "should update stream" do
-    patch stream_url(@stream, subdomain: channel),
-          params: { stream: { initial_duration: @stream.initial_duration, starts_at: @stream.starts_at,
-                              twitch_channel_id: @stream.twitch_channel_id } }
-    assert_redirected_to stream_url(@stream, subdomain: channel)
-  end
-
-  it "should destroy stream" do
-    assert_difference("Stream.count", -1) do
-      delete stream_url(@stream, subdomain: channel)
+    it "should show stream" do
+      get stream_url(@stream, subdomain: channel)
+      assert_response :success
     end
 
-    assert_redirected_to streams_url(subdomain: channel)
+    it "should get edit" do
+      get edit_stream_url(@stream, subdomain: channel)
+      assert_response :success
+    end
+
+    it "should update stream" do
+      patch stream_url(@stream, subdomain: channel),
+            params: { stream: { initial_duration: @stream.initial_duration, starts_at: @stream.starts_at,
+                                twitch_channel_id: @stream.twitch_channel_id } }
+      assert_redirected_to stream_url(@stream, subdomain: channel)
+    end
+
+    it "should destroy stream" do
+      assert_difference("Stream.count", -1) do
+        delete stream_url(@stream, subdomain: channel)
+      end
+
+      assert_redirected_to streams_url(subdomain: channel)
+    end
+
+    describe "#index" do
+      it "shows the stream"
+    end
   end
 end
