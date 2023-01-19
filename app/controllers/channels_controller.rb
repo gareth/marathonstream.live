9759ce965fd8496b67e70f3915394cb5add8861b
Gareth Adams <g@rethada.ms>
2023-01-19 22:54:02 +0000

    Allow channels to be autocreated in localhost

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,6 +1,17 @@
 class ChannelsController < ApplicationController
   include Channelable
 
+  rescue_from Channelable::NoChannelError do |exception|
+    # TODO: Remove this handler when account creation is sorted
+    if request.local?
+      Twitch::Channel.create(twitch_id: rand(1_000_000), username: subdomain, display_name: subdomain)
+
+      redirect_to root_url
+    else
+      rescue_action_without_handler(exception)
+    end
+  end
+
   def show
     @stream = twitch_channel.streams.active.first
 
