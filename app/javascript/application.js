00ca30130834aee48099a9eea0e14d227b38d100
Gareth Adams <g@rethada.ms>
2023-01-20 23:13:04 +0000

    Improve development subdomain display
    Ensure that the subdomain is hidden on more kinds of page load.
    
    Contains some more Turbo-related confusion but this whole thing is temporary and can't work outside of development

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,14 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 
-document.addEventListener("turbo:render", function () {
+const hideSudo = function () {
   const url = new URL(window.location);
   const params = url.searchParams;
   if (params.has("_sudo")) {
     params.delete("_sudo");
     window.history.replaceState(null, "", url.toString());
   }
-});
+};
+
+document.addEventListener("turbo:render", hideSudo);
+document.addEventListener("turbo:load", hideSudo);
