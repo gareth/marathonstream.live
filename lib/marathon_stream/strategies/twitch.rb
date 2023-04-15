04f2041b92cbf7a51e69dd4bca851f495eb65bdc
Gareth Adams <g@rethada.ms>
2023-04-15 11:25:48 +0100

    Improve method for removing parent gem default OAuth scope

--- lib/marathon_stream/strategies/twitch.rb
+++ lib/marathon_stream/strategies/twitch.rb
@@ -3,16 +3,9 @@ require "omniauth-oauth2"
 module MarathonStream
   module Strategies
     class Twitch < OmniAuth::Strategies::Twitch
-      def authorize_params
-        # Customize scope management
-        #
-        # OmniAuth::Strategies::Twitch defaults scopes to user:read:email and
-        # doesn't appear to give a great way to override this - particularly if
-        # you want *no* scope (which we do by default).
-        super.tap do |params|
-          params.delete(:scope)
-        end
-      end
+      # By default, ask for no scopes (The omniauth-twitch default is
+      # user:read:email)
+      option :scope, ""
     end
   end
 end
