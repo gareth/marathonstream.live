79ce538f7ed2e120a5b84ddac29a746a6d5f55a3
Gareth Adams <g@rethada.ms>
2023-05-01 01:52:01 +0100

    Link Twitch::User to their channel

--- app/models/twitch/user.rb
+++ app/models/twitch/user.rb
@@ -3,6 +3,8 @@ module Twitch
     validates :uid, presence: true
     validates :login, presence: true
 
+    has_one :channel, class_name: "Twitch::Channel", foreign_key: :twitch_id, primary_key: :uid, required: false
+
     def to_s
       format("#<%<klass>s login=%<login>s scope=%<scope>p>", klass: self.class, login:, scope: token_scopes)
     end
--- test/models/twitch/user_test.rb
+++ test/models/twitch/user_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe Twitch::User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "is linked to a channel by UID" do
+    channel = create(:twitch_channel, twitch_id: 20_001)
+    user = create(:twitch_user, uid: 20_001)
+
+    assert_equal channel, user.reload.channel
+  end
 end
